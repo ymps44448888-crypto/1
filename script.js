33416192773ef3a326331dd862be2c28eed7ed8f
ymps44448888-crypto <ymps44448888@gmail.com>
2026-03-25 11:36:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ let step = [];
 let highlight = null;
   
 for(let i = 1;i < 10;i ++){
-  const grid = document.createElememt('div');
+  const grid = document.createElement('div');
   grid.classList.add('grid')
   grid.id = i;
-  for(int j = 1;j < 10;j ++){
+  for(let j = 1;j < 10;j ++){
+    const cell = document.cratElement('div');
     cell.classList.add('cell') //小格子 空
-    cell.addEventListener('click',founction(){
-      if(i == area[area.length - 1] && cell.className = 'cell' || round == 1){
+    cell.addEventListener('click',function(){
+      if(i === area[area.length - 1] && cell.className === 'cell' || round === 1){
         area.push(j);
         showarea(j);
         this.className = 'cell';
@@ -49,7 +50,7 @@ function showarea(){
     });
   }
   if(next_area != null){
-    hightlight = next_area.childran;
+    highlight = next_area.children;
 
     Array.from(highlight).forEach(item =>{
       if(item.className == 'cell'){
